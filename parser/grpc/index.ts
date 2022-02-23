a39f40fa21fc607914b42ef5eb46de5eef02de56
ryuash <ryuash123@gmail.com>
2022-02-23 15:10:47 +0800

    reference here for async await unary call

--- parser/grpc/index.ts
+++ parser/grpc/index.ts
@@ -1,6 +1,9 @@
+/* eslint-disable */
 import 'dotenv/config';
 import * as grpc from '@grpc/grpc-js';
 import { ServiceClient as BaseClient } from '@proto/cosmos/base/tendermint/v1beta1/query_grpc_pb';
+import { QueryClient as StakingClient } from '@proto/cosmos/staking/v1beta1/query_grpc_pb';
 
 // base/tendermint
 export const baseClient = new BaseClient(process.env.GRPC as string, grpc.credentials.createSsl());
+export const stakingClient = new StakingClient(process.env.GRPC as string, grpc.credentials.createSsl());
--- parser/index.ts
+++ parser/index.ts
@@ -1,35 +1,39 @@
 import 'dotenv/config';
-import util from 'util';
-import { baseClient } from '@grpc/client';
+import * as R from 'ramda';
+import util, { promisify } from 'util';
+import { baseClient, stakingClient } from '@grpc/client';
 import { GetLatestBlockRequest } from '@proto/cosmos/base/tendermint/v1beta1/query_pb';
-import {
-  initialReset,
-  addBlocksToQueue,
-  assignBlocksToWorkers,
-} from './init';
-
-// On start reset all block and tx that are `in_progress` back to incompete
-
-// Start the cron jobs (so we don't need to deal with a web socket)
+import { QueryValidatorDelegationsRequest } from '@proto/cosmos/staking/v1beta1/query_pb';
+import { PageRequest } from '@proto/cosmos/base/query/v1beta1/pagination_pb';
 
 const start = async () => {
-  console.log('in here');
-  // prepare selected modules to a list [] modules
-  // each module will have handle block/ handle tx/ handle msg etc
-  // pass it in to assign blocks to workers
+  const test = new QueryValidatorDelegationsRequest();
+  const pageRequest = new PageRequest();
+  pageRequest.setLimit(10);
+  test.setValidatorAddr('desmosvaloper1e4g9807ephy5t7zzt6vu0kw7tryqh9k3mreujc');
+  test.setPagination(pageRequest);
 
-  // await initialReset();
-  // await addBlocksToQueue();
-  // assignBlocksToWorkers();
-
-  baseClient.getLatestBlock(new GetLatestBlockRequest(), (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const showAll = result?.toObject();
-    console.log(util.inspect(showAll, false, null, true /* enable colors */));
+  const clientgetProfile = (options: any) => new Promise((resolve, reject) => {
+    stakingClient.validatorDelegations(options, (error, response) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(response?.toObject());
+    });
   });
+
+  const emptyArr = Array(15).fill(0);
+
+  const callShit = emptyArr.map(async (x, i) => {
+    try {
+      await clientgetProfile(test);
+      console.log(`index: ${i} completed`);
+    } catch (error) {
+      console.log(`index ${i} error: ${error}`);
+    }
+  });
+
+  const data = await Promise.allSettled(callShit);
 };
 
 start();
